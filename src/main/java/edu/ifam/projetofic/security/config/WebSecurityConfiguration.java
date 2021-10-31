e08acd2bfe41af28917888a6dea0257cf5c8896f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-10-31 12:02:53 -0300

    security in process...

---
+++ src/main/java/edu/ifam/projetofic/security/config/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package edu.ifam.projetofic.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	private static final String[] PUBLIC_MATCHERS = {
+		"/h2-console/**",
+		"/categorias/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+	}
+}
